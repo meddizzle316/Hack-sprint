62024560a88fb64e27b9598c23f5ad01a258ddba
meddizzle316 <meddizzle316@gmail.com>
2024-04-11 19:19:36 +0000

    just saving as I can't test

--- index.js
+++
@@ -1,2 +0,0 @@
-const canvas = document.querySelector('canvas');
-
---
+++ static/scripts/index.js
@@ -0,0 +1,109 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const startGameBtn = document.querySelector('#startGameBtn')
+const modalEl = document.querySelector('#modalEl')
+
+class Player {
+    constructor (position, radius, color, speed) {
+        
+        // the 'coordinates' of the circle
+        this.position = position;
+
+        // the color
+        this.color = color;
+        
+        // if r*2, gives us both height and width of object from this.position location 
+        this.radius = radius;
+
+        // overall speed of object, a multiplier to velocity which moves the object
+        this.speed = speed;
+
+        // used to move the object, changing x moves horizontally, y vertically
+        this.velocity = {
+            x: 0,
+            y: 0,
+        }
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+
+    update() {
+        this.draw();
+        this.postion.x += (this.velocity.x * this.speed);
+        this.postion.y += (this.velocity.y * this.speed);
+
+
+        // condition if object reaches bottom of screen, will this even happen? 
+        // DOES not cover sides or top border collisions yet
+        if (this.position.y + (this.radius * 2) + this.velocity.y > canvas.height) {
+            this.velocity.y = 0;
+        }
+
+    }
+}
+
+const middleX = canvas.width / 2;
+const middleY = canvas.height / 2;
+
+let player = new Player({middleX, middleY}, 30, 'white', 1);
+// player.draw()
+
+function init() {
+    player = new Player({middleX, middleY}, 30, 'white', 1);
+}
+
+
+function animate() {
+    animationId = requestAnimationFrame(animate);
+    // every frame, fills canvas with black background
+    c.fillStyle = 'rgba(0, 0, 0, 0.1';
+    c.fillRect(x, y, canvas.width, canvas.height)
+
+    // every frame, draws player using draw function
+
+    player.update()
+    // TODO: condition for ending game, likely a timer or the "death" of the world
+    // ending the game
+    let placeHolder = 1; // just a placeholder to make sure we don't end yet
+    if (placeHolder == 0)    {
+        cancelAnimationFrame(animationId);
+        modalEl.style.display = 'flex';
+    }
+        
+}
+
+window.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'd':
+           player.velocity.x = 1;
+        break
+        case 'a':
+            player.velocity.x = -1;
+        break
+
+        case 's':
+            player.velocity.y = 1;
+        break
+
+        case 'w':
+            player.velocity.y = -1;
+        break
+    }
+})
+
+startGameBtn.addEventListener('click', () => {
+    init();
+    animate();
+    modalEl.style.display = 'none';
+})
+
+

c4bee971889349dd9932a5bfa3002d7451e5b8d4
meddizzle316 <meddizzle316@gmail.com>
2024-04-11 16:39:10 +0000

    accidentally started on main

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def display_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True) 
